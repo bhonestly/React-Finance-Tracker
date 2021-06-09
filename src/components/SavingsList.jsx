4dfdf9193620fd988adff2527f4371d0fa4d025e
bhonestly <ernestbizzell@gmail.com>
2021-06-09 18:27:30 +0300

    new savings  data rendering to page

--- src/components/SavingsList.jsx
+++ src/components/SavingsList.jsx
@@ -8,12 +8,15 @@ const defaultObject = {
   amount: "",
 }
 
-export default function SavingsList() {
+export default function SavingsList({data}) {
   const [input, setInput] = useState(defaultObject)
 
   const handleChange = (event) => {
-    const { name, value } = event.target
+    let { name, value } = event.target
     console.log(event.target.title, event.target.value)
+    if (name === "amount") {
+      value = event.target.valueAsNumber
+    }
 
     setInput((prevInput) => ({
       ...prevInput,
@@ -27,8 +30,21 @@ export default function SavingsList() {
     console.log(result)
   }
 
+  const savingData = data.filter(item => item.fields.type === "savings")
+  console.log(savingData)
+
   return (
     <div>
+      {savingData.map(item => {
+        return(
+          <div>
+            <h3>{item.fields.title}</h3>
+            <p>{new Date(item.fields.date).toLocaleString()}</p>
+            <p>{item.fields.amount}</p>
+          </div>
+        )
+      })}
+
     <form onChange={handleChange} onSubmit={handleSubmit}>
       <label>Name:</label>
       <input type="text" name="title"/>
@@ -37,10 +53,10 @@ export default function SavingsList() {
       <input type="text" name="type"/>
       <br />
       <label>Date:</label>
-      <input type="text" name="date"/>
+      <input type="date" name="date"/>
       <br />
       <label>Amount:</label>
-      <input type="text" name="amount"/>
+      <input type="number" name="amount"/>
       <br />
       <button type="submit">Create Savings Item</button>
       </form>
