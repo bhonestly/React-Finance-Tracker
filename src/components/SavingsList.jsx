c5731238200bd9e9c877e4ff1a75b33f6b1a4125
bhonestly <ernestbizzell@gmail.com>
2021-06-11 07:30:31 +0300

    savings modal working

--- src/components/SavingsList.jsx
+++ src/components/SavingsList.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { createSavingsItem } from '../services/api';
+import { editItem, deleteItem } from '../services/api'
 
 const defaultObject = {
   title: "",
@@ -8,8 +9,11 @@ const defaultObject = {
   amount: "",
 }
 
-export default function SavingsList({ data }) {
+export default function SavingsList({ data, reload }) {
   const [input, setInput] = useState(defaultObject)
+  const [openModal, setOpenModal] = useState(false)
+  const [modalData, setModalData] = useState("")
+  const [id, setId] = useState("")
 
   const handleChange = (event) => {
     let { name, value } = event.target
@@ -24,6 +28,17 @@ export default function SavingsList({ data }) {
     }))
   }
 
+  const handleEditChange = (event) => {
+    let { name, value } = event.target
+    if (name === "amount") {
+      value = event.target.valueAsNumber
+    }
+    setModalData((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }))
+  }
+
   const handleSubmit = async (event) => {
     event.preventDefault()
     const result = await createSavingsItem(input)
@@ -32,6 +47,19 @@ export default function SavingsList({ data }) {
 
   const savingData = data.filter(item => item.fields.type === "savings")
   console.log(savingData)
+
+  const handleEditSubmit = async (event) => {
+    event.preventDefault()
+    const result = await editItem(id, modalData)
+    reload()
+    setOpenModal(false)
+  }
+
+  const handleDelete = () => {
+    deleteItem(id)
+    reload()
+    setOpenModal(false)
+  }
 
   return (
     <>
@@ -54,13 +82,31 @@ export default function SavingsList({ data }) {
       </div>
       {savingData.map(item => {
         return (
-          <div className="finance-card">
+          <>
+          <div onClick={() => {
+            setOpenModal(true)
+            setModalData(item.fields)
+            setId(item.id)
+            }} 
+            className="finance-card">
             <h3>{item.fields.title}</h3>
             <p>{new Date(item.fields.date).toLocaleString()}</p>
             <p>{item.fields.amount}</p>
           </div>
+          </>
         )
       })}
-    </>
-  )
+      {openModal? 
+      <div className="modal">
+        <form onSubmit={handleEditSubmit} onChange={handleEditChange}>
+        <input name="title" type="text" value={modalData.title}/>
+        <input name="date" type="date" value={modalData.date}/>
+        <input name="amount" type="number" value={modalData.amount}/>
+        <button>Edit</button>
+        </form>
+        <button onClick={handleDelete}>Delete</button>
+      </div>
+      :""}
+  </>
+)
 }
