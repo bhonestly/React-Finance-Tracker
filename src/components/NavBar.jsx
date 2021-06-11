6635f62a7b6aaa96dbf70cc3b566c70b6ace8755
bhonestly <ernestbizzell@gmail.com>
2021-06-11 08:09:52 +0300

    changes to nav and total line css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,21 +5,35 @@ export default function NavBar() {
   return (
     <div className="nav">
       <ul>
-        <Link to="/incomes">
-          <li>Incomes</li>
-        </Link>
-        <Link to="/expenses">
-          <li>Expenses</li>
-        </Link>
-        <Link to="/budget">
-          <li>Budget</li>
-        </Link>
-        <Link to="/savings">
-          <li>Savings</li>
-        </Link>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
+        <div className="inner-navs">
+          <Link to="/incomes">
+            <li>Incomes</li>
+          </Link>
+        </div>
+        <div className="inner-navs">
+          <Link to="/expenses">
+            <li>Expenses</li>
+          </Link>
+        </div>
+        <div className="inner-navs">
+          <Link to="/budget">
+            <li>Budget</li>
+          </Link>
+        </div>
+        <div className="inner-navs">
+          <Link to="/savings">
+            <li>Savings</li>
+          </Link>
+        </div>
+      </ul>
+      <ul>
+        <div className="home-nav">
+          <div className="inner-home-nav">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+          </div>
+        </div>
       </ul>
     </div>
   )
